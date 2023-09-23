94aef0ee59fa2c0ad986006bfa69d3a08d585287
paolacostxx <paolacosta599@gmail.com>
2023-09-23 16:06:35 -0300

    Adição do AlunoController

---
+++ WebApplication1/Controllers/AlunoController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class AlunoController : Controller
+    {
+        // GET: Aluno
+        public ActionResult Index()
+        {
+            return View(AlunoModel.CriarLista());
+        }
+
+        // GET: Aluno/Create
+        public ActionResult Details(int id)
+        {
+            var aluno = AlunoModel.CriarLista()[id];
+            return View(aluno);
+        }
+
+        // POST: Aluno/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(AlunoModel collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Aluno/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Aluno/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Aluno/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Aluno/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ WebApplication1/Models/AlunoModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class AlunoModel
+    {
+        public string Nome { get; set; }
+        public string Curso { get; set; }
+        public DateTime Matricula { get; set; }
+        public  int Faltas { get; set; }
+
+        public static List<AlunoModel> CriarLista()
+
+        {
+            var lista = new List<AlunoModel>();
+            lista.Add(new AlunoModel() { Nome = "Nicolas", Curso = "DS", Matricula = DateTime.Now, Faltas = 5 });
+            lista.Add(new AlunoModel() { Nome = "Emerson", Curso = "DS", Matricula = DateTime.Now, Faltas = 5 });
+            lista.Add(new AlunoModel() { Nome = "Ana", Curso = "DS", Matricula = DateTime.Now, Faltas = 5 });
+            lista.Add(new AlunoModel() { Nome = "Vinicius", Curso = "DS", Matricula = DateTime.Now, Faltas = 5 });
+
+            return lista;
+        }
+    }
+
+}
